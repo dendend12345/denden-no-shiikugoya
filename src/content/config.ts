d55d39513f7bf6bd8d73b61513cb2f2070f6e052
dendend12345 <2dendenden2@gmail.com>
2026-01-21 16:14:15 +0900

    Restart from scratch

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+			title: z.string(),
+		pubDate: z.date(),
+	}),
+});
+
+const news = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+  }),
+});
+
+export const collections = { blog, news };
