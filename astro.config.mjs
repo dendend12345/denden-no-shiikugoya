3723a765feb18638ca263b5160592c991bd912a4
denden <69514964+dendend12345@users.noreply.github.com>
2026-04-01 18:50:57 +0900

    Configure site and base in astro.config.mjs
    Set site and base URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   server: {
     host: true,
+    site: 'https://<username>.github.io',
+    base: '/repo-name', // リポジトリ名をスラッシュで囲む
     watch: {
       usePolling: true,
       interval: 100,
